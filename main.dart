2ae918679f2c270e8f08ee4129959812a93f1b3d
marsalanansari <muhammadarsalanansari786@gmail.com>
2022-03-18 05:10:18 +0500

    assin4 almost

---
+++ main.dart
@@ -0,0 +1,59 @@
+void main() {
+  // ======== quastin no 1 =========
+
+// waiting for answer
+
+  // ======== quastin no 2 =========
+  // var list = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+  // List<dynamic> even = [];
+  // List<dynamic> odd = [];
+  // var num = 10;
+  // for (var i = 0; i <= 10; i++) {
+  //   if (list[i].isEven) {
+  //     even.add(list.elementAt(i));
+  //   } else {
+  //     odd.add(list.elementAt(i));
+  //   }
+  // }
+
+  // ======== quastin no 3 =========
+
+  // sawal hi samujh nhi aa raha to kese hall karo
+  // ye prime kya he ???????
+
+  // ======== quastin no 4 =========
+
+  // for (var i = 0; i <= 10; i++) {
+  //   print('7  *  $i  =  ${7 * i}');
+  // }
+
+  // ======== quastin no 5 =========
+
+  // var fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry'];
+
+  // for (var b = 0; b <= fruits.length; b++) {
+  //   print(fruits[b]);
+  // }
+
+  // ======== quastin no 6 =========
+
+  // for (var c = 1; c <= 100; c++) {
+  //   print('5  *  $c   =   ${c * 5}');
+  // }
+
+
+
+  // ======== quastin no 7 =========
+
+
+  // ye sawal bhi samujh nhi aya
+
+
+
+
+
+  // ======== quastin no 8 =========
+
+
+  
+}
